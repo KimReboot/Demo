0daa14f2c88833f4b18766dda2dc6f833f8807fa
KimReboot <yihailiu@126.com>
2014-04-03 23:53:45 +0800

    Test LMAppDelegate

---
+++ LMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LMAppDelegate.h
+//  MyTest
+//
+//  Created by mac on 14-4-3.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
